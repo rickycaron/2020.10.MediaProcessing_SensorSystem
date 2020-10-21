dafe30f10608ab6d7adee561bf8fe9a9f48be4bb
rui.zhu <rui.zhu@student.kuleuven.be>
2020-10-21 18:31:41 +0200

    solve version problem

--- sharedLib_a6/factory.h
+++ sharedLib_a6/factory.h
@@ -9,7 +9,6 @@ class Factory
 {    
 public:
     Factory();
-<<<<<<< HEAD
     Factory(string);
     string getFactoryName() const;
     void setFactoryName(const string &value);
@@ -17,12 +16,6 @@ public:
     std::unique_ptr<MotionSensor> createMotionSensor(tm activationTime,tm deactivationTime,int restrication);
     std::unique_ptr<MotionSensor> createMotionSensor(bool);
     std::unique_ptr<GasSensor> createGasSensor(string) ;
-=======
-   virtual std::shared_ptr<SmokeSensor> createSmokeSensor() =0;
-   virtual std::shared_ptr<MotionSensor> createMotionSensor(tm activationTime,tm deactivationTime,int restrication)=0;
-   virtual std::shared_ptr<GasSensor> createGasSensor(string)=0;
-
->>>>>>> cdf6c2b75b6a44a9b95b48b89e32c8c5dc28566c
 private:
     string factoryName;
 };
